10ee33499295221622dfad7fb1560065ba331240
Timur Gafiulin <nem00700@gmail.com>
2017-12-25 17:20:50 +0300

    add redux, react-redux, redux-logger and folders for redux

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Search from './components/Search';
 
 export default class App extends Component{
 
-    
     render(){
         return <div>
             <Search />
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 
 
 export default class Search extends Component {
-
     constructor(props){
         super(props);
 
