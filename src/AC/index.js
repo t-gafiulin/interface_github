ef7b48145e2c28fedffe51a3da7fe0eee0554ca0
Timur Gafiulin <nem00700@gmail.com>
2017-12-29 12:06:19 +0300

    add redux-thunk

---
+++ src/AC/index.js
@@ -0,0 +1,2 @@
+import { LOAD_ISSUES_ERROR, LOAD_ISSUES_REQUEST, LOAD_ISSUES_SUCCESS } from '../constants/index';
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 
 const logger = createLogger({
     level: "info",
     collapsed: true,
   });
 
-export default createStore(reducer, applyMiddleware(logger));
+export default createStore(
+  reducer, 
+  applyMiddleware(
+    logger,
+    thunkMiddleware
+  )
+);
