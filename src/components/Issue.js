98041cb0d238f0c5d35f730b9587791c0d819297
Timur Gafiulin <nem00700@gmail.com>
2018-01-27 19:31:24 +0300

    add header-title

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -36,25 +36,44 @@ class Issue extends Component {
 
         
         return loading ? <Loader /> :  
-            <div class='issue-page'>
-                <Link to="/"><button class='back-button'>Back to Search</button></Link>
-                <div class='user'>
-                    <img class='user__image' src={src} alt="" width='300px' height='300px'/><br/>
-                    <a class='user__link' href={author_url}>
-                        <button class='user__github-button'>{author_name} GitHub profile</button>
-                    </a>   
-                </div>
-                <div class='issue-info'>
-                    <div class='issue-info__number'>#{issue.number}</div>
-                    <div class='issue-info__title'>{issue.title}</div>
-                    <div class='issue-info__state'>{issue.state}</div>
-                    <div class='labels'>
-                        labels
+            <div class='container'>
+                <div class='issue-body'>
+                    <div class='issue-header-show'>
+                        <div class='issue-header-show__button'>
+                            <Link to="/"><button>Back</button></Link>
+                        </div>
+                        <h1 class='issue-header-show__title'>
+                            <span class='issue-header-show__text-header'>{issue.title}</span>
+                            <span class='issue-header-show__number'> #{issue.number}</span>
+                        </h1>
                     </div>
-                    <div class='issue-info__created'>{issue.created_at}</div>
+                    <div class='issue-header-meta'>
+                        <div class='issue-header-meta__state'></div>
+                        <div class='issue-header-meta__author'></div>
+                        <div class='issue-header-meta__date'></div>
+                        <div class='issue-header-meta__comments'></div>
+                    </div>
                 </div>
-                <div class='issue-body clear' dangerouslySetInnerHTML={this.bodyParser(body_issue)} />
-            </div>;
+            </div>
+            // <div class='container'>
+            //     <Link to="/"><button class='back-button'>Back to Search</button></Link>
+            //     <div class='user'>
+            //         <img class='user__image' src={src} alt="" width='300px' height='300px'/><br/>
+            //         <a class='user__link' href={author_url}>
+            //             <button class='user__github-button'>{author_name} GitHub profile</button>
+            //         </a>   
+            //     </div>
+            //     <div class='issue-info'>
+            //         <div class='issue-info__number'>#{issue.number}</div>
+            //         <div class='issue-info__title'>{issue.title}</div>
+            //         <div class='issue-info__state'>{issue.state}</div>
+            //         <div class='labels'>
+            //             labels
+            //         </div>
+            //         <div class='issue-info__created'>{issue.created_at}</div>
+            //     </div>
+            //     <div class='issue-body clear' dangerouslySetInnerHTML={this.bodyParser(body_issue)} />
+            // </div>;
 
     }
 }
