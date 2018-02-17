eeb7cef897e38500ad85b2b6edbf3792a2d2c362
Timur Gafiulin <nem00700@gmail.com>
2018-02-17 08:59:38 +0300

    add right view for first and last active page from pagination block

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -8,39 +8,41 @@ export default class Pagination extends Component {
     }
 
     getPageButtons(pages, activePageNumber, handleClick){
-
-        if(activePageNumber === 1){
-            return <div>
-                <button className='firstPrevNextLast ' disabled>First</button>
-                <button className='firstPrevNextLast ' disabled>Prev</button>
-                <button className='active' onClick={handleClick.bind(null, 1)}>1</button>
-                <button className='' onClick={handleClick.bind(null, 2)}>2</button>
-                <button className='' onClick={handleClick.bind(null, 3)}>3</button>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, activePageNumber + 1)}>Next</button>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, pages)}>Last</button>
-            </div>
-        } else if (activePageNumber === pages) {
-            return <div>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, 1)}>First</button>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, pages - 1)}>Prev</button>
-                <button className='' onClick={handleClick.bind(null, pages - 2)}>{pages - 2}</button>
-                <button className='' onClick={handleClick.bind(null, pages - 1)}>{pages - 1}</button>
-                <button className='active' onClick={handleClick.bind(null, pages)}>{pages}</button>
-                <button className='firstPrevNextLast ' disabled>Next</button>
-                <button className='firstPrevNextLast ' disabled>Last</button>
-            </div>
-        } else {
-            return <div>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, 1)}>First</button>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, activePageNumber - 1)}>Prev</button>
-                <button className='' onClick={handleClick.bind(null, activePageNumber - 1)}>{activePageNumber - 1}</button>
-                <button className='active' onClick={handleClick.bind(null, activePageNumber)}>{activePageNumber}</button>
-                <button className='' onClick={handleClick.bind(null, activePageNumber + 1)}>{activePageNumber + 1}</button>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, activePageNumber + 1)}>Next</button>
-                <button className='firstPrevNextLast ' onClick={handleClick.bind(null, pages)}>Last</button>
-            </div>
-        }
-        
+        return <div>
+            <button 
+                className='firstPrevNextLast ' 
+                disabled={activePageNumber === 1}
+                onClick={handleClick.bind(null, 1)}
+            >First</button>
+            <button 
+                className='firstPrevNextLast ' 
+                disabled={activePageNumber === 1}
+                onClick={handleClick.bind(null, activePageNumber - 1)}
+            >Prev</button>
+            <button 
+                className='' 
+                onClick={handleClick.bind(null, activePageNumber - 1)}
+                hidden={activePageNumber === 1}
+            >{activePageNumber - 1}</button>
+            <button 
+                className='active' 
+                onClick={handleClick.bind(null, activePageNumber)}>{activePageNumber}</button>
+            <button 
+                className='' 
+                onClick={handleClick.bind(null, activePageNumber + 1)}
+                hidden={activePageNumber === pages}
+            >{activePageNumber + 1}</button>
+            <button 
+                className='firstPrevNextLast ' 
+                onClick={handleClick.bind(null, activePageNumber + 1)}
+                disabled={activePageNumber === pages}
+            >Next</button>
+            <button 
+                className='firstPrevNextLast ' 
+                onClick={handleClick.bind(null, pages)}
+                disabled={activePageNumber === pages}
+            >Last</button>
+        </div>      
     }
 
     render() {
